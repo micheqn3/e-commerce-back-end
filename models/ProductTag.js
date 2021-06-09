4210ffcc238e39958175e978aebec11499958a5f
Michelle Nguyen <michellenguyen11239@aol.com>
2021-06-09 16:49:29 -0700

    Installed packages, added models, and connected sequelize

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: { // References the Product model's ID. 
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    }, 
+    tag_id: { // References the Tag model's ID.
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', 
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag). Allows products to have multiple tags
+Product.belongsToMany(Tag, {
+  through: ProductTag
+})
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag). Allows tags to have multiple products
+Tag.belongsToMany(Product, {
+  through: ProductTag
+})
 
 module.exports = {
   Product,
